4df4e029c36e4645ec0f9b7837f2af1d6abd6b16
andriytok <andriy.tokovyy@gmail.com>
2026-09-11 14:22:38 +0300

    task 2 add comments

--- apps/computers/views.py
+++ apps/computers/views.py
@@ -24,13 +24,14 @@ from apps.computers.serializers import ComputerSerializer
 from apps.computers.filters import filter_computer
 
 class ComputerListCreateView(ListCreateAPIView):
+    # ListCreateAPIView - автоматично дає list() та create() функції, правильні статус-коди та серіалізацію
     serializer_class = ComputerSerializer
 
     def get_queryset(self):
         request = self.request # отримуємо поточний http-запит
         return filter_computer(request.query_params) # повертаємо відфільтрований qs
 
-class ComputerRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+class ComputerRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView): # same але функції retrieve(), update(), destroy()
     serializer_class = ComputerSerializer
     queryset = ComputerModel.objects.all() #базовий qs, з якого django знаходить потрібний обʼєкт (за pk з url)
     http_method_names = ['get', 'put', 'patch', 'delete'] # дозволені для цього view http-методи
